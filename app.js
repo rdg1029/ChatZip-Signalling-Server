6987a39b75356d69111d90aac659a9b178db6a21
rdg1029 <wcle3456@gmail.com>
2021-07-11 15:11:21 +0900

    Update app.js : Edit room connection

--- app.js
+++ app.js
@@ -33,12 +33,30 @@ io.on('connection', socket => {
         io.to(roomId).emit('req info', userId);
     })
 
+    socket.on('room info', (targetId, users) => {
+        io.to(targetId).emit('room info', users);
+    });
+
+    socket.on('req offer', (roomId, userId) => {
+        io.to(roomId).emit('req offer', userId);
+    });
+
     socket.on('req answer', (offer, userId, targetId) => {
         io.to(targetId).emit('req answer', offer, userId);
     });
     
     socket.on('recv answer', (answer, userId, targetId) => {
         io.to(targetId).emit('recv answer', answer, userId);
+    })
+
+    socket.on('conn ready', targetId => {
+        io.to(targetId).emit('conn ready');
+    });
+
+    socket.on('req join', roomId => {
+        io.to(socket.id).emit('join room', roomId);
+        io.to(roomId).emit('user join', socket.id);
+        socket.join(roomId);
     })
 
     socket.on('disconnect', () => {
